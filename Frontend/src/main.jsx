8098095afc5454e029257f7fc95ff1ff89e5b4ff
Daksh Goplani <dakshgoplani7@gmail.com>
2026-07-08 11:31:34 +0530

    feat: remove inline highlight.js imports from index.html and move to main.jsx for smoother deployment

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'remixicon/fonts/remixicon.css'
+import "highlight.js/styles/nord.css";
 
 createRoot(document.getElementById('root')).render(
     <App />
